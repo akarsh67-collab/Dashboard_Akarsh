81a54fd5f910b0366f0e3d9f7dec701c02683b02
akarsh67-collab <akarshraj465@gmail.com>
2026-01-02 12:47:56 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,59 @@
+let timeDisplay = document.querySelector('.timeDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+let msec = 0;
+let secs = 0;
+let mins = 0;
+
+let timeId = null;
+
+
+startBtn.addEventListener('click',function(){
+if(timeId !==
+  null){
+  clearInterval(timeId);
+}
+timeId = setInterval(startTimer,10);
+
+});
+
+stopBtn.addEventListener('click',function(){
+clearInterval(timeId);
+timeId = null;
+});
+
+resetBtn.addEventListener('click',function(){
+  
+  clearInterval(timeId);
+  timeId = null;
+  msec = 0;
+  secs = 0;
+  mins = 0;
+  timeDisplay.innerHTML= '00 : 00 : 00';
+  
+  });
+
+function startTimer(){
+  msec++;
+  if(msec === 100){
+    msec = 0;
+    secs++;
+    if(secs === 60){
+      secs = 0;
+      mins++;
+    }
+  }
+  let msecString = msec < 10 ?`0${msec}` : msec;
+  let secsString = secs < 10 ?`0${secs}` : secs;
+  let minsString = mins < 10 ?`0${mins}` : mins;
+
+  timeDisplay.innerHTML=`${minsString} : ${secsString} : ${msecString}`;
+  /*let msecString;
+  if(msec < 10){
+    msecString =`0${msec}`;
+  }
+  else{
+    msecString = msec;
+  }*/
+}
